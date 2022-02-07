ce6cb7cd82f76256d015ef4a853fc77c0daa2622
Noah Rowe <nrrowe@mun.ca>
2022-02-07 00:59:26 -0330

    Fixed fog system to work as two numpy arrays

--- the_hybrid.py
+++ the_hybrid.py
@@ -54,14 +54,14 @@ class FOWChess:
 
     def __init__(self, chess_game=None, color=None):
         self.board = chess.Board() if chess_game is None else chess_game
-        self._color = color
+        # self._color = color
 
-    def swap_color(self):
-        self._color = not self._color
-
-    @property
-    def color(self):
-        return self._color if self._color is not None else self.board.turn
+    # def swap_color(self):
+    #     self._color = not self._color
+    #
+    # @property
+    # def color(self):
+    #     return self._color if self._color is not None else self.board.turn
 
     @cached_property
     def moves(self):
@@ -85,44 +85,10 @@ class FOWChess:
         # Iterate through all squares, convert to an (8x8) array repr
         # Unnecessary list comp for speed gains
         board_array = np.reshape(
-            [piece_repr(self.board.piece_at(square))
-                for square in (chess.SQUARES_180 if self.color else self._SQUARES_MIRRORED_LR)],
-            (8, 8))
+            [piece_repr(self.board.piece_at(square)) for square in chess.SQUARES_180],
+                (8, 8))
 
-        return board_array if self.color else np.fliplr(board_array)
-
-    @cached_property
-    def board_array(self):
-        """
-        Creates an 8x8 numpy array with each piece type represented by its
-        corresponding number in cls.PIECE_ENCODING.
-        Note; arrays are oriented with the side whose turn it is down
-        (like a chess board) and black sides are mirrored vertically
-        such that a starting white and black board look the same.
-        This is done for neural network consistency.
-
-        Parameters
-        ----------
-
-        Returns
-        -------
-        numpy.ndarray
-            (8,8) array representation of a chess board.
-
-        """
-
-        # Given a chess.Piece, either return it's cls.PIECE_ENCODING value or 0
-        def piece_repr(piece):
-            return self.piece_encoding()[piece.symbol()] if piece else 0
-
-        # Iterate through all squares, convert to an (8x8) array repr
-        # Unnecessary list comp for speed gains
-        board_array = np.reshape(
-            [piece_repr(self.board.piece_at(square))
-                for square in (chess.SQUARES_180 if self.color else self._SQUARES_MIRRORED_LR)],
-            (8, 8))
-
-        return board_array if self.color else np.fliplr(board_array)
+        return np.dstack((np.flip(board_array, 0) * -1, board_array))
 
     @cached_property
     def fog(self):
@@ -136,8 +102,11 @@ class FOWChess:
 
         """
         # Convert the int64 (bit-board) mask to a 8x8 numpy mask array
-        mask = bitboard_to_numpy(self._visable_squares())
-        return mask if self.color else np.flipud(mask)
+
+        return np.dstack((
+                np.flip(bitboard_to_numpy(self._visable_squares(False)), (0)),
+                bitboard_to_numpy(self._visable_squares(True)),
+                 ))
 
     @property
     def foggy_board(self):
@@ -172,14 +141,18 @@ class FOWChess:
         """
         Simple conversion for printing.
         """
-        chess.Board().__repr__()
-        return np.array2string(self.board_array) + "\n" + str(self.board.turn) + "\n"
+        # chess.Board().__repr__()
+        return f"""
+            WHITE POV: \n {np.array2string(self.board_array[:,:,1])} \n  
+            BLACK POV: \n {np.array2string(self.board_array[:,:,0])} \n
+            Current turn: {self.board.turn}
+            """
 
     def __str__(self):
         """
         Simple conversion for debugging.
         """
-        return np.array2string(self.board_array)
+        return self.__repr__()
 
     def make_move(self, move):
         """
@@ -229,15 +202,15 @@ class FOWChess:
         for attr in ("fog", "moves", "board_array"):
             self.__dict__.pop(attr, None)
 
-    def piece_encoding(self):
-        if self._color is None:
-            color = True
-        else:
-            color = self._color
+    # def piece_encoding(self):
+    #     if self._color is None:
+    #         color = True
+    #     else:
+    #         color = self._color
+    #
+    #     return {k: v * (-1 if not color else 1) for k, v in self.DEFAULT_ENCODING.items()}
 
-        return {k: v * (-1 if not color else 1) for k, v in self.DEFAULT_ENCODING.items()}
-
-    def _visable_squares(self):
+    def _visable_squares(self, color):
         """
         Run through each piece type's move patterns to find all possible moves.
 
@@ -249,7 +222,7 @@ class FOWChess:
         """
         visable = 0
 
-        our_pieces = self.board.occupied_co[self.color]
+        our_pieces = self.board.occupied_co[color]
         visable |= our_pieces
 
         # Generate non-pawn moves.
@@ -264,13 +237,13 @@ class FOWChess:
         if pawns:
             # First if they can attack anyone
             pawn_attacks = reduce_with_bitwise_or(
-                chess.BB_PAWN_ATTACKS[self.color][frm] & self.board.occupied_co[not self.color]
+                chess.BB_PAWN_ATTACKS[color][frm] & self.board.occupied_co[not color]
                 for frm in chess.scan_reversed(pawns)
             )
             visable |= pawn_attacks
 
             # Then find their single and double moves
-            if self.color == chess.WHITE:
+            if color == chess.WHITE:
                 single_moves = pawns << 8 & ~self.board.occupied
                 double_moves = (
                     single_moves << 8 & (chess.BB_RANK_3 | chess.BB_RANK_4) & ~self.board.occupied
@@ -293,4 +266,15 @@ class FOWChess:
 
 
 if __name__ == "__main__":
-    pass
+    a = FOWChess()
+    print(a)
+    print(a.board)
+    for _ in range (40):
+        a.make_random_move()
+    print(a.board)
+    print(a)
+    print("w")
+    b = a.foggy_board
+    print(b[:,:,1])
+    print("b")
+    print(b[:,:,0])
