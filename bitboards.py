f6c967ff9da9c62f15cf9dec9bdc7b9da3005df7
Noah Rowe <nrrowe@mun.ca>
2022-02-20 17:47:44 -0330

    moved import

--- bitboards.py
+++ bitboards.py
@@ -1,5 +1,6 @@
-from collections.abc import Iterator, Iterable
 from __future__ import annotations
+
+from collections.abc import Iterator, Iterable
 from functools import reduce
 from typing import NamedTuple
 
