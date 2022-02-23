3b7c5105375c994ec367ef3b9129b80fd7e38fe1
Noah Rowe <nrrowe@mun.ca>
2022-02-22 23:17:07 -0330

    Added stubs of AttackMasks' unit tests

---
+++ test_attack_masks.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+from  attack_masks import *
+
+class TestAttackMasks(TestCase):
+    def test_square_distance(self):
+        self.fail()
+
+    def test_sliding_moves(self):
+        self.fail()
+
+    def test_step_moves(self):
+        self.fail()
+
+    def test_pawn_attacks(self):
+        self.fail()
+
+    def test_knight_moves(self):
+        self.fail()
+
+    def test_king_moves(self):
+        self.fail()
+
+    def test_rank_moves(self):
+        self.fail()
+
+    def test_file_moves(self):
+        self.fail()
+
+    def test_diagonal_moves(self):
+        self.fail()
+
+    def test_piece_move_mask(self):
+        self.fail()
